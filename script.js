d23c585a6504814e3e376e6add685aae50111295
Yasir Eqbal <140412597+nsaky@users.noreply.github.com>
2025-09-20 18:49:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
+
+hamburgerMenu.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
